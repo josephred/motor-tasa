dc5e954d7e6bb6873536ff5fd9df95f2d2860fc5
Aaron G <agarate@outlook.cl>
2021-08-24 16:32:50 -0400

    se agrega boton auditoria y monitoreo

--- menu_vertical.php
+++ menu_vertical.php
@@ -47,6 +47,27 @@ $pagina = str_replace("/", "", $url);
 
 
 
+          
+          <?php if( $_SESSION["perfil"]=="Administrador" ||  $_SESSION["perfil"]=="Auditor"  ){?>
+          <li class="<?php echo $pagina == 'estadisticas.php' ? 'active' : ''; ?>">
+            <a href="auditoria.php">
+              <i class="fa fa-line-chart"></i>
+              <div style="margin-left: 30px;">Auditoría</div>
+            </a>
+          </li>
+          <? } ?>
+          
+          <?php if( $_SESSION["perfil"]=="Administrador" || $_SESSION["perfil"] == "Monitor" ){?>
+            <li class="<?php echo $pagina == 'estadisticas.php' ? 'active' : ''; ?>">
+              <a href="monitoreo.php">
+                <i class="fa fa-pie-chart"></i>
+                <div style="margin-left: 30px;">Monitoreo</div>
+              </a>
+            </li>
+          <? } ?>
+
+
+
          
         </ul>
       </div>
